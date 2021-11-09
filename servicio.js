0516bdec273fbaf08abf114f39f0f018b40bf579
laloek <laloek15@gmail.com>
2021-11-08 20:07:53 -0800

    Error login resuelto

--- servicio.js
+++ servicio.js
@@ -1,20 +1,23 @@
 const express= require('express')
 const path= require('path')
-const fs=require('fs')
+const fs = require('fs')
+const flash = require("express-flash")
 const formidable= require('formidable')
 const session= require('express-session')
 const  routerIndex =require('./routes/index')
 const  routerProductos =require('./routes/productos')
 const  tiendaWeb =require('./routes/tienda_web')
-
+const routerAuth = require('./routes/auth')
 const cookieParser=require('cookie-parser')
 const port = 3000
+
 var app = express();
 
 // Configuracion de engine de vistas en carpeta view de EJS
 //npm install ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(flash())
 
 const DOMINIO_PERMITIDO_CORS = "http://localhost:4200",
   DIRECTORIO_FOTOS = path.join(__dirname, "fotos_productos"),
@@ -23,7 +26,14 @@ const DOMINIO_PERMITIDO_CORS = "http://localhost:4200",
 
   app.use("/foto_producto", express.static(DIRECTORIO_FOTOS));
   // Estático
-  app.use("/", express.static(DIRECTORIO_DIST));
+app.use("/", express.static(DIRECTORIO_DIST));
+app.use(
+  session({
+    secret: '123456abc',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 600000 }
+  }))
   
   if (!fs.existsSync(DIRECTORIO_FOTOS)) {
     fs.mkdirSync(DIRECTORIO_FOTOS);
@@ -42,8 +52,10 @@ app.use(cookieParser());
 //Contenido estatico
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/crud',routerIndex)
-app.use('/',tiendaWeb)
-app.use('/productos',routerProductos)
+app.use('/tw', tiendaWeb)
+app.use('/auth', routerAuth)
+app.use('/productos', routerProductos)
+
 app.listen(port,function(){
 
     console.log('Example app ruta:localhost:'+port)
